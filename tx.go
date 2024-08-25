399a095b16f316bfe32f36eab598565220407076
Vladislav Yarmak <vladislav@vm-0.com>
2024-08-25 21:48:41 +0300

    WIP

--- tx.go
+++ tx.go
@@ -1,10 +1,48 @@
 package lfmap
 
+import (
+	"iter"
+
+	"github.com/benbjohnson/immutable"
+)
+
 type Tx[K comparable, V any] interface {
 	Delete(key K)
 	Get(key K) (value V, ok bool)
 	Len() int
-	Set(key K, value V)
 	Range(yield func(key K, value V) bool)
+	Set(key K, value V)
 }
 
+var _ Tx[string, string] = &tx[string, string]{}
+var _ iter.Seq2[string, string] = Tx[string, string](nil).Range
+
+type tx[K comparable, V any] struct {
+	m *immutable.Map[K, V]
+}
+
+func (t *tx[K, V]) Delete(key K) {
+	t.m = t.m.Delete(key)
+}
+
+func (t *tx[K, V]) Get(key K) (value V, ok bool) {
+	return t.m.Get(key)
+}
+
+func (t *tx[K, V]) Len() int {
+	return t.m.Len()
+}
+
+func (t *tx[K, V]) Set(key K, value V) {
+	t.m = t.m.Set(key, value)
+}
+
+func (t *tx[K, V]) Range(yield func(key K, value V) bool) {
+	itr := t.m.Iterator()
+	for !itr.Done() {
+		k, v, _ := itr.Next()
+		if !yield(k, v) {
+			return
+		}
+	}
+}

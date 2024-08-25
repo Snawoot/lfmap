9a97e5fc9724aa8f21d392209178b604782e5bed
Vladislav Yarmak <vladislav@vm-0.com>
2024-08-25 23:55:30 +0300

    tx doc

--- tx.go
+++ tx.go
@@ -6,12 +6,27 @@ import (
 	"github.com/benbjohnson/immutable"
 )
 
+// Tx is a handle to map state usable from within transactions.
+// It's methods are NOT safe for concurrent use.
 type Tx[K comparable, V any] interface {
+	// Clears the map.
 	Clear()
+
+	// Deletes the key.
 	Delete(key K)
+
+	// Returns the value for a given key and a flag indicating whether the key
+	// exists. This flag distinguishes a nil value set on a key versus a
+	// non-existent key in the map.
 	Get(key K) (value V, ok bool)
+
+	// Returns the number of elements in the map.
 	Len() int
+
+	// Map iterator suitable for use with range keyword.
 	Range(yield func(key K, value V) bool)
+
+	// Updates the map setting specified key to the new value.
 	Set(key K, value V)
 }
 

96a428379bc6a0eabe9222a312b373b3bf86e789
Vladislav Yarmak <vladislav@vm-0.com>
2026-04-30 23:33:46 +0300

    use maphash from stdlib

--- hasher.go
+++ hasher.go
@@ -1,17 +1,21 @@
 package lfmap
 
 import (
-	"github.com/dolthub/maphash"
+	"hash/maphash"
 )
 
-type hasher[K comparable] maphash.Hasher[K]
+type hasher[K comparable] struct{
+	seed maphash.Seed
+}
 
 func newHasher[K comparable]() hasher[K] {
-	return hasher[K](maphash.NewHasher[K]())
+	return hasher[K]{
+		seed: maphash.MakeSeed(),
+	}
 }
 
 func (h hasher[K]) Hash(key K) uint32 {
-	return uint32(maphash.Hasher[K](h).Hash(key))
+	return uint32(maphash.Comparable[K](h.seed, key))
 }
 
 func (h hasher[K]) Equal(a, b K) bool {
